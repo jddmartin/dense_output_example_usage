9b9a745d12a80800605c5c28b8508004ff9e52f0
James D. D. Martin <jddmartin@uwaterloo.ca>
2015-02-28 15:54:53 -0500

    Modifications for compliance with PEP8.

--- examples/complex_lorenz.py
+++ examples/complex_lorenz.py
@@ -1,9 +1,8 @@
-"""
-Integration of complex Lorenz equations to demonstrate dense output 
+"""Integration of complex Lorenz equations to demonstrate dense output
 capabilities of the DOP853 ode solver code.
 
 The goal of this example is to reproduce Fig. 1 from
-  Fowler, Gibbon and McGuinness,  "The complex Lorenz equations", 
+  Fowler, Gibbon and McGuinness,  "The complex Lorenz equations",
   Physica 4D, 139-163 (1982)
   http://dx.doi.org/10.1016/0167-2789(82)90057-4
 showing the advantage of dense output.
@@ -11,93 +10,98 @@ showing the advantage of dense output.
 
 from __future__ import division, print_function, absolute_import
 
-import math, numpy
+import math
+import numpy
 from scipy.integrate import ode, complex_ode, dense_dop
 from collections import namedtuple
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class ComplexLorenz(object):
     def __init__(self, b, sigma, r1, r2, e):
-        self.b=b
-        self.sigma=sigma
-        self.r=r1+1.0j*r2
-        self.a=1.0-1.0j*e
+        self.b = b
+        self.sigma = sigma
+        self.r = r1+1.0j*r2
+        self.a = 1.0-1.0j*e
 
     def f(self, t, v):
         """Complex Lorenz system odes, Eq. 1.1 of Fowler et al. (see above).
         """
-        (x, y, z) = v 
+        (x, y, z) = v
         return [-self.sigma*x+self.sigma*y,
                 -x*z+self.r*x-self.a*y,
                 -self.b*z+(x.conjugate()*y).real]
 
+
 class SolOut(object):
     def __init__(self, tbounds=None, tinc=0.1):
         if tbounds is not None:
-            Bounds=namedtuple("bounds", "min max")
-            self.tbounds=Bounds(tbounds[0], tbounds[1])
+            Bounds = namedtuple("bounds", "min max")
+            self.tbounds = Bounds(tbounds[0], tbounds[1])
         else:
-            self.tbounds=None
-        self.sparse_output=[]
-        self.dense_output=[]
-        self.tinc=tinc
+            self.tbounds = None
+        self.sparse_output = []
+        self.dense_output = []
+        self.tinc = tinc
 
     def solout(self, nr, told, t, v, con_view, icomp):
         x, y, z = v
-        if nr == 1: # initial starting point:
+        if nr == 1:  # at initial starting point:
             self.sparse_output.append((t, x.real, y.real, z.real,))
             self.dense_output.append((t, x.real, y.real, z.real,))
-            self.tdense=t+self.tinc
-        else: # subsequent step positions (after starting point):
+            self.tdense = t+self.tinc
+        else:  # subsequent step positions (after starting point):
             if self.tbounds:
                 if ((t < self.tbounds.min) or (t > self.tbounds.max)):
                     return
-            self.sparse_output.append((t,x.real,y.real,z.real,))
+            self.sparse_output.append((t, x.real, y.real, z.real,))
             while t > self.tdense:
                 xd, yd, zd = dense_dop(self.tdense, told, t, con_view)
-                self.dense_output.append((self.tdense,xd.real,yd.real,zd.real))
+                self.dense_output.append((self.tdense,
+                                          xd.real, yd.real, zd.real))
                 self.tdense += self.tinc
+
 
 if __name__ == "__main__":
     # Figure 1 and 2:
-    system=ComplexLorenz(4.0/3.0, 2.0, 2.0, 1.0, 3.0)
-    aSolOut=SolOut(tinc=0.01)
+    system = ComplexLorenz(4.0/3.0, 2.0, 2.0, 1.0, 3.0)
+    aSolOut = SolOut(tinc=0.01)
 
     # initial conditions, and length of time to integrate:
-    t0=0.0
-    v0=[1.0, 0.0, 0.0]
-    tend=10.0
+    t0 = 0.0
+    v0 = [1.0, 0.0, 0.0]
+    tend = 10.0
 
     # desired tolerances:
-    itol=0
-    rtol=1.0e-7
-    atol=rtol
+    itol = 0
+    rtol = 1.0e-7
+    atol = rtol
 
-    ig = complex_ode(system.f).set_integrator('dop853', atol=atol, rtol=rtol, 
+    ig = complex_ode(system.f).set_integrator('dop853', atol=atol, rtol=rtol,
                                               nsteps=1000000)
-    ig.set_solout(aSolOut.solout, dense_components=(0,1,2))
+    ig.set_solout(aSolOut.solout, dense_components=(0, 1, 2))
     ig.set_initial_value(v0, t0)
 
     # solve system:
     ret = ig.integrate(tend)
 
     # plot dense and sparse outputs, emulating Fowler et al.'s Fig. 1.
-    dense_output_array=np.asarray(aSolOut.dense_output)
-    sparse_output_array=np.asarray(aSolOut.sparse_output)
-    for plotnum, second_dataset in ((1,sparse_output_array),
-                                    (2,dense_output_array)):
-        plt.subplot(2, 1, plotnum, aspect=3.6/2.1, 
-                    xlim=[-1.8,1.8], ylim=[-0.9,1.20])
-        plt.plot(sparse_output_array[:,2], sparse_output_array[:,1],'+')
-        plt.plot(second_dataset[:,2], second_dataset[:,1])
-        plt.ylabel("real(X)") # unconventional X and Y ordering in paper!
-        plt.xticks(np.arange(-1,2, 1.0))
+    dense_output_array = np.asarray(aSolOut.dense_output)
+    sparse_output_array = np.asarray(aSolOut.sparse_output)
+    for plotnum, second_dataset in ((1, sparse_output_array),
+                                    (2, dense_output_array)):
+        plt.subplot(2, 1, plotnum, aspect=3.6/2.1,
+                    xlim=[-1.8, 1.8], ylim=[-0.9, 1.20])
+        plt.plot(sparse_output_array[:, 2], sparse_output_array[:, 1], '+')
+        plt.plot(second_dataset[:, 2], second_dataset[:, 1])
+        plt.ylabel("real(X)")  # unconventional X and Y ordering in paper!
+        plt.xticks(np.arange(-1, 2, 1.0))
         plt.minorticks_on()
-        for point in (0,-1):
-            plt.text(sparse_output_array[point,2],
-                     sparse_output_array[point,1],
-                     ('t=%.2f' % sparse_output_array[point,0]),fontsize=8,
+        for point in (0, -1):
+            plt.text(sparse_output_array[point, 2],
+                     sparse_output_array[point, 1],
+                     ('t=%.2f' % sparse_output_array[point, 0]), fontsize=8,
                      verticalalignment='bottom', horizontalalignment='center')
     plt.xlabel("real(Y)")
 
--- examples/van_der_pol.py
+++ examples/van_der_pol.py
@@ -1,80 +1,84 @@
-"""Solution of van der Pol differential equation illustrating usage of the 
-dense output option of the DOP853 integrator.
+"""Solution of van der Pol differential equation illustrating usage of the
+ dense output option of the DOP853 integrator.
 
 An introductory discussion is given on pages 111 of Hairer et al.'s
-"Solving Ordinary Differential Equations, Nonstiff Problems", 
+"Solving Ordinary Differential Equations, Nonstiff Problems",
 Second Revised Edition, Springer, 1993.
 but epsilon has a different definition here (reciprocal of the book's usage).
 
-This code is modelled after 
-"Driver for DOP853 on Van der Pol's equation"
+This code is modelled after "Driver for DOP853 on Van der Pol's equation"
 available at: http://www.unige.ch/~hairer/prog/nonstiff/dr_dop853.f
-(note that Hairer's source code comments accidentally mislabels this 
+(note that Hairer's source code comments accidentally mislabels this
 file as a driver for DOPRI5, rather than DOP853).
 
-Since being incorporated into scipy, the DOP853 code has been updated by Hairer 
-to have an "iout=3" dense option, which eliminates wasteful calculation of 
-interpolation polynomials in regimes where dense output is not required.  
-So although the following code reproduces the output of "dr_dop853.py", it 
-does so with more function evaluations (compare the last line of output with 
-fortran code that uses "iout=3").
+Since being incorporated into scipy, the DOP853 code has been updated by Hairer
+to have an "iout=3" dense option, which eliminates wasteful calculation of
+interpolation polynomials in regimes where dense output is not required.
+So although the following code reproduces the output of "dr_dop853.py", it
+does so with more function evaluations (compare the last line of output with
+Fortran code that uses "iout=3").
 """
 
 from __future__ import division, print_function, absolute_import
 
-import math, numpy
+import math
+import numpy
 from scipy.integrate import ode, complex_ode, dense_dop
 
-def f_van_der_pol(x, y, rpar):
-    """The system of differential equations.
-    """
-    eps=rpar[0]
-    return [y[1], ((1-y[0]**2)*y[1]-y[0])/eps]
-
-# parameters for differential equation system:
-rpar=numpy.zeros((1,),float)
-rpar[0]=1.0e-3
-
-# initial conditions, and length of time to integrate:
-x0=0.0
-y0=[2.0, 0.0]
-xend=2.0
-
-# desired tolerances:
-itol=0
-rtol=1.0e-9
-atol=rtol
-
-xout = 0.0 # used to keep track of current time in "solout".
 
 def solout(nr, xold, x, y, con_view, icomp):
-    global xout # You can avoid use of "global" by using classes (recommended).
-                # i.e. make solout a member function, and xout a class attribute.
-                # (see "complex_lorenz.py" as an example).
+    global xout
+    # Note that the use of "global" can be avoided using classes
+    # (recommended). i.e. make solout a member function, and xout a class
+    # attribute (see "complex_lorenz.py" as an example).
+
     # mimic Fortran output format:
-    format_string=" X = {0:5.2f}   Y ={1:18.10e}{2:18.10e}    NSTEP = {3:4d}"
+    format_string = (" X = {0:5.2f}   Y ={1:18.10e}{2:18.10e}    "
+                     "NSTEP = {3:4d}")
     if nr == 1:
         print(format_string.format(x, y[0], y[1], nr-1))
         xout = 0.1
     else:
         while x >= xout:
-            dense=dense_dop(xout, xold, x, con_view)
+            dense = dense_dop(xout, xold, x, con_view)
             print(format_string.format(xout, dense[0], dense[1], nr-1))
             xout += 0.1
 
-ig = ode(f_van_der_pol).set_integrator('dop853', atol=atol, rtol=rtol, 
+
+def f_van_der_pol(x, y, rpar):
+    """The system of differential equations.
+    """
+    eps = rpar[0]
+    return [y[1], ((1-y[0]**2)*y[1]-y[0])/eps]
+
+# parameters for differential equation system:
+rpar = numpy.zeros((1,), float)
+rpar[0] = 1.0e-3
+
+# initial conditions, and length of time to integrate:
+x0 = 0.0
+y0 = [2.0, 0.0]
+xend = 2.0
+
+# desired tolerances:
+itol = 0
+rtol = 1.0e-9
+atol = rtol
+
+xout = 0.0  # used to keep track of current time in "solout".
+
+ig = ode(f_van_der_pol).set_integrator('dop853', atol=atol, rtol=rtol,
                                        nsteps=10000)
 
 # Now use the new dense output option by specification of "dense_components"
 # in call of ".set_solout" method:
-ig.set_solout(solout, dense_components=(0,1,))
+ig.set_solout(solout, dense_components=(0, 1,))
 
 ig.set_initial_value(y0, x0).set_f_params(rpar)
 ret = ig.integrate(xend)
 
 print(" X = XEND    Y ={0:18.10e}{1:18.10e}".format(ret[0], ret[1],))
 
-iw=ig._integrator.returned_iwork
+iw = ig._integrator.returned_iwork
 print("        tol={0:5.2e} \n fcn= {1:d} step= {2:d} "
-      "accpt= {3:d} rejct= {4:d}".format(atol,*iw[16:20]))
-
+      "accpt= {3:d} rejct= {4:d}".format(atol, *iw[16:20]))
